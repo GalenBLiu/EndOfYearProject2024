45687850e011221d2abac06dccbebedfd997c27c
nicye25 <yenick2007@gmail.com>
2024-05-21 12:33:43 -0400

    simple script

--- WeatherGetter.py
+++ WeatherGetter.py
@@ -1,22 +1,20 @@
 from datetime import datetime
 import matplotlib.pyplot as plt
-from meteostat import Point, Daily, Hourly
+from meteostat import Point, Daily
 
 # Set time period
-startingHour = 0;
-endingHour = 0;
-
-start = datetime(2024, 5, 21, startingHour)
-end = datetime(2024, 5, 21, endingHour)
-
+start = datetime(2018, 1, 1)
+end = datetime(2018, 1, 2)
+print(type(start))
 # Create Point for Vancouver, BC
-hackensack = Point(40.8838, -74.0555, 70)
+vancouver = Point(49.2497, -123.1193, 70)
 
-# Get hourly data
-data = Hourly(hackensack, start, end, 'EST', True, False)
+# Get daily data for 2018
+data = Daily(vancouver, start, end)
 data = data.fetch()
-#print(str(data))
 
 # Plot line chart including average, minimum and maximum temperature
-#data.plot(y=['temp'])
-#plt.show()
+data.plot(y=['tavg', 'tmin', 'tmax'])
+print(type(data))
+print(data.head())
+# plt.show()
---
+++ simple1.py
@@ -0,0 +1,27 @@
+# Simple script to check if the average temp is lower than 0 on a given day
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+from meteostat import Point, Daily
+
+#Get time stamp data
+year = int(input("What year? "))
+month = int(input("What month? "))
+day = int(input("What day? "))
+
+ts = datetime(year, month, day)
+print(ts)
+
+#Start and end are the same because 1 day only
+start = end = ts
+
+#Define location using coordinates
+academies = Point(40.90243696271137, -74.0344921768194)
+data = Daily(academies, start, end)
+
+#Fetch data in pd Dataframe
+data = data.fetch()
+print(data)
+
+#If average temp on the day is < 0, FREEZING!
+
