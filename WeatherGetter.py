0982d2041e4e98703d79c4880b0ad44ec973ac7d
nicye25 <yenick2007@gmail.com>
2024-05-24 09:34:37 -0400

    added .yml environment file

--- WeatherGetter.py
+++ WeatherGetter.py
@@ -3,28 +3,15 @@ import matplotlib.pyplot as plt
 from meteostat import Point, Daily
 
 # Set time period
-<<<<<<< Updated upstream
 start = datetime(2018, 1, 1)
 end = datetime(2018, 1, 2)
 print(type(start))
-=======
-startingHour = 0;
-endingHour = 0;
-
-start = datetime(2024, 5, 21, 12)
-end = datetime(2024, 5, 21, 18)
-
->>>>>>> Stashed changes
 # Create Point for Vancouver, BC
 vancouver = Point(49.2497, -123.1193, 70)
 
 # Get daily data for 2018
 data = Daily(vancouver, start, end)
 data = data.fetch()
-<<<<<<< Updated upstream
-=======
-print(str(data))
->>>>>>> Stashed changes
 
 # Plot line chart including average, minimum and maximum temperature
 data.plot(y=['tavg', 'tmin', 'tmax'])
--- simple1.py
+++ simple1.py
@@ -7,12 +7,17 @@ from meteostat import Point, Daily
 #Get time stamp data
 year = int(input("What year? "))
 month = int(input("What month? "))
-day = int(input("What day? "))
+d = int(input("What day? "))
 
-ts = datetime(year, month, day)
+ts = datetime(year, month, d)
 
 #Start and end are the same because 1 day only
-start = end = ts
+start = ts
+print(start)
+end = ts.replace(month=3)
+print(end)
+
+
 
 #Define location using coordinates
 academies = Point(40.90243696271137, -74.0344921768194)
@@ -20,8 +25,13 @@ data = Daily(academies, start, end)
 
 #Fetch data in pd Dataframe
 data = data.fetch()
-avg_temp = data.at[ts, 'tavg']
-print(avg_temp)
-
+print(data)
+# avg_temp = data.at[ts, 'tavg']
+# snow = data.at[ts, 'snow']
+# print("avg_temp:" + str(avg_temp))
+# print('snow:' + str(snow))
+print(data.loc[:, 'snow'])
+for x in data.index:
+    print(data.loc[x, 'snow'])
 #If average temp on the day is < 0, FREEZING!
 
