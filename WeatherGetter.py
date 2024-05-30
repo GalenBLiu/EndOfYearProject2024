a922e145098a87828349f932e93eb0561b87bf12
GalenBLiu <galliu25@bergen.org>
2024-05-30 09:59:36 -0400

    Update WeatherGetter.py

--- WeatherGetter.py
+++ WeatherGetter.py
@@ -1,15 +1,15 @@
 from datetime import datetime
 import matplotlib.pyplot as plt
-from meteostat import Point, Daily, Hourly
+from meteostat import Point, Hourly
 
 # Set time period
-start = datetime(2018, 3, 13, 0)
-end = datetime(2018, 3, 15, 0)
+start = datetime(2024, 5, 12, 0)
+end = datetime(2020, 5, 12, 20)
 print(type(start))
-# Create Point for Vancouver, BC
-Hackensack = Point(49.2497, -123.1193, 70)
 
-# Get daily data for 2018
+Hackensack = Point(40.8859, -74.0435, 8)
+
+# Get hourly data for 2018
 data = Hourly(Hackensack, start, end)
 data = data.fetch()
 print(data)
