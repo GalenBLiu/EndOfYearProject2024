2eef5234ec997b75bd1620a1532a598a745bca4e
IBeCoding26 <96630166+IBeCoding26@users.noreply.github.com>
2024-06-04 12:14:40 -0400

    New csv

--- percentageModel.py
+++ percentageModel.py
@@ -1,42 +1,56 @@
+from matplotlib import pyplot as plt
+import numpy as np
 import pandas as pd
-import sklearn
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 data = pd.read_csv('delays.csv', index_col=0)
 print(data.head())
 print(data.groupby('Snow Day').count())
 print(data.isnull().sum())
 
-X = data.drop(columns=['Snow Day', 'Date'], axis = 1)
+X = data.drop(['Snow Day', 'Date'], axis=1)
 y = data['Snow Day']
-#Check double quotes
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0, shuffle=True)
+# y_train = y_train.values.reshape(-1,1)
+# y_test = y_test.values.reshape(-1,1)
+ 
 print("X_train shape:",X_train.shape)
 print("X_test shape:",X_test.shape)
+print(type(X_test))
+print(X_test)
 print("y_train shape:",y_train.shape)
 print("y_test shape:",y_test.shape)
 
-clf = RandomForestClassifier()  
-clf.fit(X_train, y_train)
+result_dict_train = {}
+result_dict_test = {}
 
-predictions = clf.predict(X_test)
+rfc = RandomForestClassifier(random_state = 42)
+accuracies = cross_val_score(rfc, X_train, y_train, cv=5)
+rfc.fit(X_train,y_train)
+y_pred = rfc.predict(X_test)
 
-print(classification_report(y_test, predictions))
+print(y_test.to_numpy())
+print(y_pred)
 
-probabilities = clf.predict_proba(X_test)
+print("RFC Train Score:",np.mean(accuracies))
+print("Test Score:",rfc.score(X_test,y_test))
 
-
-#Gets probabilities based off index
-# print("Probability: ", probabilities[20])
-
-for i in range(150):
-    print(i, "Probability: ", probabilities[i])
-
-
-
-
-
+result_dict_train["Random Forest Train Score"] = np.mean(accuracies)
+result_dict_test["Random Forest Test Score"] = rfc.score(X_test,y_test)
