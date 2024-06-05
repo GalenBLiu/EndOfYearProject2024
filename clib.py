484a0fc203c8c2478d831345d8587de4d3951e39
nicye25 <yenick2007@gmail.com>
2024-06-04 21:23:26 -0400

    made script to poll data from forecast and make prediction

--- clib.py
+++ clib.py
@@ -1,4 +1,9 @@
 import requests
+import openmeteo_requests
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+from meteostat import Point, Daily
 
 API_TOKEN = 'VLVtaEUDWkLeHsqoCNJaTiDFYqZHRMGd'
 BASE_URL = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/'
@@ -24,3 +29,53 @@ def get_historical_data(station_id, start_date, end_date, datatypeid):
     else:
         print(f"Error: {response.status_code}")
         return None
+
+def get_params():
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 40.90243696271137,
+        "longitude": -74.0344921768194,
+        "daily": ["temperature_2m_max", "temperature_2m_min", "snowfall_sum"],
+        "timezone": "America/New_York",
+        "forecast_days": 2
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    # print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    # print(f"Elevation {response.Elevation()} m asl")
+    # print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    # print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+    daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+    daily_snowfall_sum = daily.Variables(2).ValuesAsNumpy()
+
+    daily_data = {"date": pd.date_range(
+        start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+        end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = daily.Interval()),
+        inclusive = "left"
+    )}
+    daily_data["temperature_2m_max"] = daily_temperature_2m_max
+    daily_data["temperature_2m_min"] = daily_temperature_2m_min
+    daily_data["snowfall_sum"] = daily_snowfall_sum
+
+    return daily_snowfall_sum, daily_temperature_2m_min, daily_temperature_2m_max
+
+def get_tavg(ts):
+    start = end = ts
+    academies = Point(40.90243696271137, -74.0344921768194)
+    data = Daily(academies, start, end)
+    data = data.fetch()
+    return data.at[ts, 'tavg']
--- dtc_model.py
+++ dtc_model.py
@@ -2,9 +2,15 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pickle 
 import pandas as pd
+from clib import get_params, get_tavg
+
+s, tmin, tmax = get_params()
+tavg = get_tavg(pd.to_datetime('today').normalize())
+print(s, tmin, tmax, tavg)
+
+sc = pickle.load(open('StandardScaler.pkl', 'rb'))
+predict_data = np.array([s[0], tmin[0], tmax[0], tavg])
+predict_data = np.array(sc.transform(predict_data.reshape(1,-1)))
 
 dtc = pickle.load(open('dtc_model.sav', 'rb'))
-sc = pickle.load(open('StandardScaler.pkl', 'rb'))
-predict_data = np.array([50, -0.7, 11, 3])
-predict_data = np.array(sc.transform(predict_data.reshape(1,-1)))
 print(dtc.predict(predict_data))
